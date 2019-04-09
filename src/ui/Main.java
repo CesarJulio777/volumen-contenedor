6d45e42b810ae4157b897c95cdaf2c32ea9abbc8
Juan Reyes <seyerman@gmail.com>
2019-04-09 15:37:56 -0500

    Fixed the menu options in ui

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -9,49 +9,55 @@ public class Main{
 	public static void main (String[]args){
 		
 		Scanner sc = new Scanner(System.in);
+		System.out.println("Welcome to the Container Volume Calculator! ");
 		
-			System.out.println("Welcome to the Container Volume Calculator! ");
-			System.out.println("Select an option");
+		int option = 0;
+		while(option !=3){
+			System.out.println("============");
+			System.out.println("Options Menu");
+			System.out.println("============");
 			System.out.println("1. Calculate Total Volume of the container and merchandise");
 			System.out.println("2. Calculate Average volume of 3 containers");
 			System.out.println("3. Exit");
+			System.out.print("Enter the option: ");
 		
-			int option = 0;
-				while(option !=3){
 					option = sc.nextInt();
                 
 			switch(option){
 				case 1:
-				System.out.println("Ingrese el ancho del contenedor");
-				double a = sc.nextDouble();
-				System.out.println("Ingrese el alto del contenedor");
-				double h = sc.nextDouble();
-				System.out.println("Ingrese el largo del contenedor");
-				double l = sc.nextDouble();
-				Contenedor c = new Contenedor(a,h,l);
-				System.out.println("The volume of the container is: " + c.calcularVolumendeContenedor());
+					System.out.println("Ingrese el ancho del contenedor");
+					double a = sc.nextDouble();
+					System.out.println("Ingrese el alto del contenedor");
+					double h = sc.nextDouble();
+					System.out.println("Ingrese el largo del contenedor");
+					double l = sc.nextDouble();
+					Contenedor c = new Contenedor(a,h,l);
+					System.out.println("The volume of the container is: " + c.calcularVolumendeContenedor());
 				break;
 
 				case 2:
-				double acumulado = 1;
-				double contenedores = 0;
-				System.out.println("Ingrese la cantidad de contenedores a promediar");
-				double contain = sc.nextDouble();
-				
-				while(acumulado <= contain){
-					System.out.println("Ingrese el volumen del contenedor numero: " + acumulado);
-					double cn = sc.nextDouble();
-					contenedores += cn;
-					acumulado ++;
-				}
-				
-				double promedio = contenedores/contain;
-				
-				System.out.println("The average volume of the containers is: "+ promedio);
-				
+					double acumulado = 1;
+					double contenedores = 0;
+					System.out.println("Ingrese la cantidad de contenedores a promediar");
+					double contain = sc.nextDouble();
+					
+					while(acumulado <= contain){
+						System.out.println("Ingrese el volumen del contenedor numero: " + acumulado);
+						double cn = sc.nextDouble();
+						contenedores += cn;
+						acumulado ++;
+					}
+					
+					double promedio = contenedores/contain;
+					
+					System.out.println("The average volume of the containers is: "+ promedio);				
+				break;
+				case 3:
+					System.out.println("Thank you for using this program. Good bye!");
 				break;
 
 				default:
+					System.out.println("The option "+option+" there is not in the menu");
 				break;
 			}
 		
